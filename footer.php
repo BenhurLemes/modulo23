ea130f0709b5706c7c53193f4e7e23a7b92fa035
unknown <benhurlemes1@gmail.com>
2023-09-25 11:36:15 -0300

    first commit

---
+++ footer.php
@@ -0,0 +1,13 @@
+<footer class="footer">
+    <div class="footer-custom">
+        <h5><?php bloginfo('name'); ?></h5>
+        <p><?php bloginfo('description'); ?></p>
+    </div>
+
+    <div class="footer-wp">
+        <?php wp_footer(); ?>
+    </div>
+</footer>
+
+</body>
+</html>
